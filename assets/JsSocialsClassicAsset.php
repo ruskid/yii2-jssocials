63c0d973e38a8f291ffd601bd69c9a01dd6123dc
Victor Demin <demmbox@gmail.com>
2018-08-14 02:13:16 +0300

    Update JsSocialsClassicAsset.php

--- assets/JsSocialsClassicAsset.php
+++ assets/JsSocialsClassicAsset.php
@@ -19,12 +19,10 @@ class JsSocialsClassicAsset extends AssetBundle {
 
     public $sourcePath = '@bower/jssocials';
 
-    public function init() {
-        parent::init();
-
-        $this->css[] = 'dist/jssocials-theme-classic.css';
-    }
-
+    public $css = [
+        'dist/jssocials-theme-classic.css'
+    ];
+  
     public $depends = [
         'ruskid\jssocials\assets\JsSocialsAsset'
     ];
